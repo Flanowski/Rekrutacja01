b9d1dc770d35f7336eb48a3eb3ffd6d2b1f3cca8
Flanowski <kacper.flanowski@gmail.com>
2020-08-10 19:47:41 +0200

    ,

---
+++ Notebooks/Common/CreateDatabases.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC %sql
+// MAGIC CREATE DATABASE IF NOT EXISTS srcData COMMENT 'Database stores source data'
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC CREATE DATABASE IF NOT EXISTS destData COMMENT 'Database stores source data'
